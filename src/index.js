1e54781f0902aded4f568567223daa385dd0237a
Zach North <zrnorth@gmail.com>
2018-12-10 22:13:30 +0900

    geoloc + conditional rendering

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { 
+      lat: null,
+      errorMessage: '',
+     };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    }
+    else {
+      return <div>Loading...</div>
+    }
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
