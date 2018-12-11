a3ba41c939d85880f0e22438b40ff59dd8006eed
Zach North <zrnorth@gmail.com>
2018-12-11 23:42:33 +0900

    Added the season display based on lat + month

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,39 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season display</div>;
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    iconName: 'sun',
+  },
+  winter: {
+    text: 'Brr, it\'s chilly!',
+    iconName: 'snowflake',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 
+      ? 'summer'  // northern hemisphere
+      : 'winter'; // southern hemisphere
+  }
+  else {
+    return lat > 0
+      ? 'winter'  // northern hemisphere
+      : 'summer'; // southern hemisphere
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+  return (
+  <div>
+    <i className={`massive ${iconName} icon`} />
+    <h1>{text}</h1>
+    <i className={`massive ${iconName} icon`} />
+  </div>
+  );
 }
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = { 
-      lat: null,
-      errorMessage: '',
-     };
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => this.setState({ errorMessage: err.message })
-    );
-  }
+  state = { 
+    lat: null, 
+    errorMessage: '' 
+  };
 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />;
     }
     else {
       return <div>Loading...</div>
     }
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
   }
 }
 
