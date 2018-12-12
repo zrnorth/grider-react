2ef1c1b6bb2870a73e2ec91b5b5711f695751adf
Zach North <zrnorth@gmail.com>
2018-12-12 23:44:49 +0900

    Added api call to unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import SearchBar from './SearchBar';
-class App extends React.Component {
+import unsplash from '../api/unsplash';
 
-  onSearchSubmit(term) {
-    console.log(term);
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+      params: { 
+        query: term
+      }
+    });
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
    return(
       <div className="ui container" style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found {this.state.images.length} images.
       </div>
     );
    }
