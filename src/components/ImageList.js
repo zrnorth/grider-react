cb745cbe6ab67c64aa6c6cee3e3146efb004ae92
Zach North <zrnorth@gmail.com>
2018-12-13 23:02:32 +0900

    Grid row styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   const images = props.images.map(image => {
-    return <img src={image.urls.regular} />
+    return (
+      <ImageCard key={image.id} image={image} />
+    );
   });
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 }
 
 export default ImageList;
