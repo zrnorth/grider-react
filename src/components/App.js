03c1e852b1b0fede2db488985bd4b443c519ac3f
Zach North <zrnorth@gmail.com>
2018-12-15 17:50:55 +0900

    Added video select and iframe

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoDetail from "./VideoDetail";
+import VideoList from "./VideoList";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("react");
+  }
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -11,14 +17,33 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <p>I have {this.state.videos.length} videos.</p>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
