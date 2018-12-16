0c8fc824b2c30f79a3bb64bd94f5d17bf2838750
Zach North <zrnorth@gmail.com>
2018-12-16 21:14:05 +0900

    Resetting for the pics project

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,7 @@
 import React from "react";
-import SearchBar from "./SearchBar";
-import VideoDetail from "./VideoDetail";
-import VideoList from "./VideoList";
-import youtube from "../apis/youtube";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
+const App = () => {
+  return <div>App</div>;
+};
 
-  componentDidMount() {
-    this.onTermSubmit("react");
-  }
-
-  onTermSubmit = async term => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term
-      }
-    });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0]
-    });
-  };
-
-  onVideoSelect = video => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
 export default App;
