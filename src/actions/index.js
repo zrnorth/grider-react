c162a3426a45829bc415eeb99f903e5442647597
Zach North <zrnorth@gmail.com>
2018-12-18 23:39:57 +0900

    Adding FetchUser + naive ddosey implementation

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,11 @@ export const fetchPosts = () => async dispatch => {
     payload: response.data
   });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
+};
