241c94bf5df1d922c7d0acfa9a0886aba9c12867
Zach North <zrnorth@gmail.com>
2018-12-20 22:35:24 +0900

    Scaffolding for streams app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "../components/streams/StreamCreate";
+import StreamDelete from "../components/streams/StreamDelete";
+import StreamEdit from "../components/streams/StreamEdit";
+import StreamList from "../components/streams/StreamList";
+import StreamShow from "../components/streams/StreamShow";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
