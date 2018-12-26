1c77a764a88b0649119648a1642289186c13d3ff
Zach North <zrnorth@gmail.com>
2018-12-26 20:27:21 +0900

    Added custom history and some linking

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
+import history from "../history";
 import Header from "../components/Header";
 import StreamCreate from "../components/streams/StreamCreate";
 import StreamDelete from "../components/streams/StreamDelete";
@@ -10,7 +11,7 @@ import StreamShow from "../components/streams/StreamShow";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -19,7 +20,7 @@ const App = () => {
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
