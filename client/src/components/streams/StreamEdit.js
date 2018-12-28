38de9ceba6f6d87dee4b3002831e3dcc1e827e04
Zach North <zrnorth@gmail.com>
2018-12-28 14:37:53 +0900

    componentized stream form and stubbed out edit

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm
+          initialValues={this.props.stream} // form is smart and gets title/desc from this
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
