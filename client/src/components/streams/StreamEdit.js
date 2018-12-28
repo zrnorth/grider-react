7f8732fd666b8de53198a0ec0225bd1ac20465d2
Zach North <zrnorth@gmail.com>
2018-12-28 14:49:36 +0900

    whoops, edit should be patch not put

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from "lodash";
 import { connect } from "react-redux";
 import { fetchStream, editStream } from "../../actions";
 import StreamForm from "./StreamForm";
@@ -9,7 +10,7 @@ class StreamEdit extends React.Component {
   }
 
   onSubmit = formValues => {
-    console.log(formValues);
+    this.props.editStream(this.props.match.params.id, formValues);
   };
 
   render() {
@@ -21,7 +22,7 @@ class StreamEdit extends React.Component {
       <div>
         <h3>Edit Stream</h3>
         <StreamForm
-          initialValues={this.props.stream} // form is smart and gets title/desc from this
+          initialValues={_.pick(this.props.stream, "title", "description")}
           onSubmit={this.onSubmit}
         />
       </div>
