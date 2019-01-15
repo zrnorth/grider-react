eb3eb0deedf1b1df5a37a3f431aa8211b5d83612
Zach North <zrnorth@gmail.com>
2019-01-14 16:50:11 -0800

    Added hook lifecycles and reuse

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
+import UserList from "./UserList";
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState("todos");
+
+  return (
+    <div>
+      <UserList />
+
+      <div>
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
